7d31e692c3013f0db3d498497ea71d793ad0ef76
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-20 19:49:00 +0200

    complete UI

---
+++ PassingData/PassingData/DataDetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DataDetailsViewController.swift
+//  PassingData
+//
+//  Created by Manyuchi, Carrington C on 2024/08/20.
+//
+
+import UIKit
+
+class DataDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- PassingData/PassingData/ViewController.swift
+++ PassingData/PassingData/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var emailText: UITextField!
+        @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
+    
 }
 
