b1af807fa7d334a7506866cf5b5f98207a214322
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-20 19:25:08 +0200

    startup

---
+++ PassingData/PassingData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by Manyuchi, Carrington C on 2024/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
